931d29b5a50838bccd2b69a704d1d3718a94174c
Wilmer Cantillo <will.canti2697@gmail.com>
2023-08-23 19:19:49 -0500

    sync changed module script. remove comment from setup.

--- .circleci/list-changed-modules.sh
+++ .circleci/list-changed-modules.sh
@@ -8,10 +8,17 @@ echo $input
 echo ''
 
 # Remove the square brackets and quotes from the input string
-echo 'Remove the square brackets and quotes from the input string'
 cleaned_input=$(echo "$input" | tr -d '[]"' | tr ',' '\n')
-echo $cleaned_input
+echo 'Cleared list of apps that changed' $cleaned_input
 echo ''
+
+# If app changed list is empty then exit.
+if [ -z "$cleaned_input" ]
+then
+  echo 'No package changed, exiting.'
+  circleci-agent step halt
+  exit
+fi
 
 # Create the output file and write the filtered and cleaned input to it
 echo "$cleaned_input" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | grep -v '//' | sed 's/^/apps\//' > "$modules_filtered"
