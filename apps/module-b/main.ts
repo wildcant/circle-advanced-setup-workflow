e1541596d229937bea2c44285e2791d07493dbfb
Wilmer Cantillo <will.canti2697@gmail.com>
2023-08-24 10:37:20 -0500

    change module b

--- apps/module-b/main.ts
+++ apps/module-b/main.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 express()
   .get('/', (_req, res) => {
-    res.send('Hello People!')
+    res.send('Hello World!')
   })
   .listen(3000, () => {
     console.log(`Example app listening on port ${3000}`)
